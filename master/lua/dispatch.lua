7d8e98db8ca5905098503aa050e5d147e976bbe1
yulong li <m@liyl.me>
2017-11-11 15:56:46 +0800

    add tag when master dispatch

--- master/lua/dispatch.lua
+++ master/lua/dispatch.lua
@@ -5,7 +5,7 @@ end
 
 local function rewrite()
     local uri = ngx.var.scheme .. "://".. get_agent_ip() .. "/" .. ngx.req.get_headers()["Host"] ..ngx.var.uri
-    local tag = "_from_node=" .. ngx.var.server_addr .. "&_ts" .. ngx.localtime()
+    local tag = "_fnode=" .. ngx.var.server_addr .. "&_ts=" .. ngx.utctime()
     if ngx.var.query_string then
         uri = uri .. "?" ..ngx.var.query_string .. "&" .. tag
     else
