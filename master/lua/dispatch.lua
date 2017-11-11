4855b6ec6edae9670fed0f45b0f4b5c3f4553f2a
yulong li <m@liyl.me>
2017-11-11 21:54:05 +0800

    set proxy http version to 1.1

--- master/lua/dispatch.lua
+++ master/lua/dispatch.lua
@@ -3,15 +3,15 @@ local function get_agent_ip()
     return "115.28.55.40"
 end
 
-local function rewrite()
-    local uri = ngx.var.scheme .. "://".. get_agent_ip() .. "/" .. ngx.req.get_headers()["Host"] ..ngx.var.uri
+local function dispatch()
+    local uri = ngx.var.scheme .. "://" .. get_agent_ip() .. "/" .. ngx.var.server_name .. ngx.var.uri
     local tag = "_fnode=" .. ngx.var.server_addr .. "&_ts=" .. ngx.utctime()
     if ngx.var.query_string then
-        uri = uri .. "?" ..ngx.var.query_string .. "&" .. tag
+        uri = uri .. "?" .. ngx.var.query_string .. "&" .. tag
     else
         uri = uri .. "?" .. tag
     end
     return ngx.redirect(uri)
 end
 
-rewrite()
+dispatch()
